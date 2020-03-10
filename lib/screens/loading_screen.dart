675f4bcfcf2b096286b0e42f2f5341557a1d817e
MtDeity <o8olo853q43@gmail.com>
2020-03-10 20:46:47 +0900

    JSON parsing

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:clima/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const apiKey = 'aa33a7c90cdc07fd0033a319b2a6a172';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,10 +19,29 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('Longitude: ${location.longitude}');
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+      'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22',
+    );
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print(temperature);
+      print(condition);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   @override
